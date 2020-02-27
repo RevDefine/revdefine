76c5d986ea014cc53466b4a5a84bf03f7ea2319e
Will Qiu <zsluedem06@gmail.com>
2020-02-27 17:07:19 +0800

    make websocket useful, add event

--- src/client/websocket/ws.ts
+++ src/client/websocket/ws.ts
@@ -7,13 +7,12 @@ export default class RnodeWebsocket {
   public url: string;
   public constructor(url: string) {
     this.url = url;
-    this.connect(url);
   }
 
-  public connect(url: string) {
-    if (this.ws === undefined || this.ws.readyState === WebSocket.CLOSED ) {
-      this.ws = new WebSocket(url);
-      this.onEvents()
+  public connect() {
+    if (this.ws === undefined || this.ws.readyState === WebSocket.CLOSED) {
+      this.ws = new WebSocket(this.url);
+      this.onEvents();
     } else {
       throw Error('connection not closed');
     }
@@ -23,22 +22,29 @@ export default class RnodeWebsocket {
     if (this.ws.readyState === WebSocket.OPEN || this.ws.readyState === WebSocket.CONNECTING) {
       this.ws.close(NORMALCLOSECODE);
     }
-    this.connect(this.url);
+    this.connect();
   }
 
-  private onMessage(event: Event) {}
+  private onMessage(event: MessageEvent) {
+    const data = JSON.parse(event.data);
+    const eventType = data.event;
+    const payload = data.payload;
+    if (eventType == 'block-added') {
+      EventBus.triggerEvent(REvent.BlockAdded, payload);
+    }
+  }
 
   public onEvents() {
     this.ws.onopen = function(event: Event) {
-      EventBus.triggerEvent(REvent.Open);
+      EventBus.triggerEvent(REvent.Open, {});
     };
 
     this.ws.onclose = function(event: Event) {
-      EventBus.triggerEvent(REvent.Close);
+      EventBus.triggerEvent(REvent.Close, {});
     };
 
     this.ws.onerror = function(event: Event) {
-      EventBus.triggerEvent(REvent.Error);
+      EventBus.triggerEvent(REvent.Error, {});
     };
 
     this.ws.onmessage = this.onMessage;
