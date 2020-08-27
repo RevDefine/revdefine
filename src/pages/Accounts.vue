307a6e3edceeb053ad2b14257854a580327a8193
Will Qiu <zsluedem06@gmail.com>
2020-08-27 11:29:44 +0800

    account use router view

--- src/pages/Accounts.vue
+++ src/pages/Accounts.vue
@@ -1,19 +1,15 @@
  <template>
   <div>
     <div class="row col-xs-12 justify-center">
-      <account-table></account-table>
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import accountTable from '../components/AccountTable.vue';
 
 export default Vue.extend({
-  components: {
-    'account-table': accountTable
-  },
   name: 'accounts',
   data() {
     return {};
