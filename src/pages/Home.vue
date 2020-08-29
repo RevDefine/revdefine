153a35049ec2918fa6a28add8629f9e11b657920
Will Qiu <zsluedem06@gmail.com>
2020-08-29 18:44:59 +0800

    chart update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,10 +6,20 @@
 
     <div class="row">
       <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6 bg-white">
-        <chart></chart>
+        <apex-chart
+          :loading="transferStatLoading"
+          :data="transferStatData"
+          title="Last 7 Days Transfer"
+          :total="transferStatTotal"
+        ></apex-chart>
       </div>
       <div class="col-xs-12 col-md-6 col-sm-6 col-lg-6 bg-white">
-        <chart></chart>
+        <apex-chart
+          :loading="deployStatLoading"
+          :data="deployStatData"
+          title="Last 7 Days Deploy"
+          :total="deployStatTotal"
+        ></apex-chart>
       </div>
     </div>
 
@@ -30,16 +40,16 @@
 <script lang="ts">
 import Vue from 'vue';
 import SearchBlockBar from '../components/SearchBar.vue';
-import Chart from '../components/ApexChart.vue';
 import lastBlockInfo from '../components/LastBlock.vue';
 import latestTransfer from '../components/LatestTransfer.vue';
+import apexChart from '../components/ApexChart.vue';
 import client from '../defineAPI';
 export default Vue.extend({
   components: {
     'search-bar': SearchBlockBar,
     'last-block-info': lastBlockInfo,
     'latest-transfer': latestTransfer,
-    chart: Chart
+    'apex-chart': apexChart
   },
   name: 'Home',
   data() {
@@ -59,7 +69,39 @@ export default Vue.extend({
           reason: ''
         }
       ],
-      loading: false
+      loading: false,
+
+      transferStatLoading: false,
+      transferStatData: [
+        {
+          name: 'transfer',
+          data: [
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0]
+          ]
+        }
+      ],
+      transferStatTotal: 0,
+
+      deployStatLoading: false,
+      deployStatData: [
+        {
+          name: 'Deploy',
+          data: [
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0],
+            [0, 0]
+          ]
+        }
+      ],
+      deployStatTotal: 0
     };
   },
   methods: {
@@ -69,10 +111,39 @@ export default Vue.extend({
       this.data = transactions.transactions;
       this.loading = false;
     },
+    async getTransferStat() {
+      this.transferStatLoading = true;
+      const stat = await client.statTransfer();
+      const data = stat.datas.map(element => [element.start, element.data]);
+      const total = stat.datas.reduce((s, element) => s + element.data, 0);
+      this.transferStatData = [
+        {
+          name: 'transfer',
+          data: data
+        }
+      ];
+      this.transferStatTotal = total;
+      this.transferStatLoading = false;
+    },
+    async getDeployStat() {
+      this.deployStatLoading = true;
+      const stat = await client.statDeploy();
+      const data = stat.datas.map(element => [element.start, element.data]);
+      const total = stat.datas.reduce((s, element) => s + element.data, 0);
+      this.deployStatData = [
+        {
+          name: 'transfer',
+          data: data
+        }
+      ];
+      this.deployStatTotal = total;
+      this.deployStatLoading = false;
+    },
     search() {}
   },
   async mounted() {
-    await this.requestLatestTransfer(1);
+    const itemsToLoad = [this.requestLatestTransfer(1), this.getTransferStat(), this.getDeployStat()];
+    await Promise.all(itemsToLoad);
   }
 });
 </script>
