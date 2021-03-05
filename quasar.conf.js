fd7dfad60eee124e8b9748a1d2decbb07344283b
Will Qiu <zsluedem06@gmail.com>
2021-03-05 14:05:44 +0800

    introduce gtag

--- quasar.conf.js
+++ quasar.conf.js
@@ -11,7 +11,7 @@ module.exports = function (ctx) {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
-    boot: ['i18n', 'timeago'],
+    boot: ['i18n', 'timeago', 'gtag'],
     supportTS: true,
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: ['app.sass'],
